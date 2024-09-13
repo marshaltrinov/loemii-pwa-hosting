84ed86628c36272541dcf8cbe6043a7ec62818c9
marshaltrinov <154526264+marshaltrinov@users.noreply.github.com>
2024-09-13 21:34:33 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'loemii-cache-v1';
 const urlsToCache = [
-  'https://script.google.com/macros/s/your-app-url/exec',
+  'https://script.google.com/macros/s/AKfycbyQl4kf0v_WYrqt1Ol0MOR2YGolf013M0YMyBU0hZ8a0q1sGjo_7AW6lu1wqBC8l_LO/exec',
 ];
 
 self.addEventListener('install', event => {
